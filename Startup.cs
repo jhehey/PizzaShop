1fd790e5fb20cd347674bab2dcc42762beff93a3
jhehey <jhonpaul.jaspe@emerson.com>
2020-06-13 14:43:28 +0800

    Add xml output formatter

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,9 @@ namespace PizzaShop
         {
             services.AddDbContextPool<PizzaShopDbContext> (opt => opt.UseInMemoryDatabase ("PizzaShop"));
             services.AddAutoMapper (AppDomain.CurrentDomain.GetAssemblies ());
-            services.AddControllers ();
+            services.AddControllers (
+                setupAction => { setupAction.ReturnHttpNotAcceptable = true; }
+            ).AddXmlDataContractSerializerFormatters ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
